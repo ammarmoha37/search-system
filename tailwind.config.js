745208f68286770ed0a254b93c5378e10a4c60eb
Ammar <101853260+ammarmoha37@users.noreply.github.com>
2024-07-06 03:43:56 +0300

    feat: UI - create sidebar component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        readex: ['"Readex Pro"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
 
